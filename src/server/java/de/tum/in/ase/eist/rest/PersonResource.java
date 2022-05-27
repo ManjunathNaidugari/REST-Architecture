c448f23c7f8ac836b79e768413e50f8015be12ca
ManjunathNaidugari <manjunath311020@gmail.com>
2022-05-27 15:57:42 +0200

    part 1

--- src/server/java/de/tum/in/ase/eist/rest/PersonResource.java
+++ src/server/java/de/tum/in/ase/eist/rest/PersonResource.java
@@ -1,9 +1,14 @@
 package de.tum.in.ase.eist.rest;
 
+import de.tum.in.ase.eist.model.Person;
 import de.tum.in.ase.eist.service.PersonService;
+import de.tum.in.ase.eist.util.PersonSortingOptions;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -16,4 +21,34 @@ public class PersonResource {
     }
 
     // TODO Part 1: Implement the specified endpoints here
+   @PostMapping("persons")
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        if (person.getId() != null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(personService.savePerson(person));
+    }
+
+    @PutMapping("persons/{personId}")
+    public ResponseEntity<Person> updatePerson(@RequestBody Person updatePerson, @PathVariable("personId")UUID personId) {
+        if (!updatePerson.getId().equals(personId)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(personService.savePerson(updatePerson));
+    }
+
+    @DeleteMapping("persons/{personId}")
+    public ResponseEntity<Void> deletePerson(@PathVariable("personId") UUID personId) {
+        personService.deletePerson(personId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("persons")
+    public ResponseEntity<List<Person>> getAllPersons(@RequestParam("secret") String secret) {
+        if (!"SecretKey".equals(secret)) {
+            return ResponseEntity.badRequest().build();
+        }
+        PersonSortingOptions sortingOptions = new PersonSortingOptions();
+        return ResponseEntity.ok(personService.getAllPersons(sortingOptions));
+    }
 }
